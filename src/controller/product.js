1eb6177b7338fa99c340de0edcaa4d12dc395bd0
Ashish Rayalwar <rayalwar.ashish.1@gmail.com>
2023-07-10 15:55:33 +0530

    new commit

--- src/controller/product.js
+++ src/controller/product.js
@@ -5,12 +5,28 @@ const createProduct = async (req, res) => {
   try {
     const data = req.body;
 
-    const productId = (await productModel.find()).length + 1;
-    data.productId = productId;
-    const create = await productModel.create(data);
-    return res
-      .status(201)
-      .json({ message: "Product saved sucesfully", data: create });
+    const category = await categoryModel.findOne({
+      categoryName: data.categoryName,
+    });
+
+    const categoryId = (await categoryModel.find().length) + 1;
+    if (!category) {
+      const len = await categoryModel.create({
+        categoryName: data.categoryName,
+        categoryId: categoryId,
+      });
+      data.categoryId = categoryId;
+      // const create = await productModel.create(data);
+      // return res.status(201).json({data:create})
+    } else {
+      const productId = (await productModel.find()).length + 1;
+
+      data.productId = productId;
+      const create = await productModel.create(data);
+      return res
+        .status(201)
+        .json({ message: "Product saved sucesfully", data: create });
+    }
   } catch (error) {
     console.log(error.message);
     return res.status(500).json({ message: error.message });
@@ -27,10 +43,15 @@ const getProducts = async (req, res) => {
     if (products.length === 0)
       return res.status(404).json({ message: "Products not found" });
 
+    const newArr = products.map((x) => {
+      const { _id, __v, createdAt, updatedAt, ...rest } = x._doc;
+      return rest;
+    });
+
     return res.status(200).json({
       categoryId: categoryId,
       categoryName: category.categoryName,
-      data: products,
+      data: newArr,
     });
   } catch (error) {
     console.log(error.message);
